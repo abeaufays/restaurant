d260d6d3bd89247012c8a4efb5b6308d584cc78f
abeaufays <beaufays.alexandre@gmail.com>
2025-10-27 23:03:15 +0100

    Implement the user related endpoints

--- src/restaurant/routers/users.py
+++ src/restaurant/routers/users.py
@@ -1,17 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from restaurant import schemas
+from restaurant import database, models, schemas
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("/")
-def list_users():
+def list_users(db: Session = Depends(database.get_db)):
     """List all users."""
-    return {"users": []}
+    users = db.query(models.User).all()
+    return {"users": users}
 
 
 @router.post("/")
-def create_user(user: schemas.User):
+def create_user(user: schemas.User, db: Session = Depends(database.get_db)):
     """Create a new user."""
-    return user
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
